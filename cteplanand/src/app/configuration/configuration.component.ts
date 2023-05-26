5b0f3331949ad29c1e6a700e3a9550af7736710b
dotnet-code <@dotnet-code>
2023-05-26 12:09:39 +0530

    commit from code202305260939

---
+++ cteplanand/src/app/configuration/configuration.component.ts
@@ -0,0 +1,18 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-configuration',
+  templateUrl: './configuration.component.html',
+  styleUrls: ['./configuration.component.css']
+})
+export class ConfigurationComponent implements OnInit {
+
+  errorMessage: string;
+
+  constructor() { }
+
+  ngOnInit() {
+   this.errorMessage = 'NoneType' object has no attribute 'replace'';
+  }
+
+}
